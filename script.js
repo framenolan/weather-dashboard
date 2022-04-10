511762c15b88ffbba02964717992791cb1013a7a
Nolan Frame <frame.nolan@gmail.com>
2022-04-10 12:29:01 -0700

    html and css added

--- script.js
+++ script.js
@@ -1,2 +1,31 @@
 console.log("connected")
 
+// Weather Dashboard Structure
+
+// Form Inputs
+//     City
+//     Search Button
+//     Save to Local Storage
+
+// Search Results
+// Current Weather
+//     City Name
+//     Date of Weather
+//     Icon representation of the conditions
+//     Temperature
+//     Humidity
+//     Wind Speed
+//     UV Index
+//         Presented with color that indicateds (favorable/moderate/severe)
+    
+// 5 Day Forecast
+//     Date
+//     Icon of conditions
+//     Temperature
+//     Wind Speed
+//     Humidity
+
+// When Click on a city in the search history
+//     shows current and 5day Results
+
+
